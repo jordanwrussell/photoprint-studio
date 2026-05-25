863ba460b4c2be5cb6db0e17ff832cd1749a738b
Jordan Russell <jwrussell83@gmail.com>
2026-05-25 16:15:23 -0700

    Add Webpack alias configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
