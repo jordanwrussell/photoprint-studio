016a7b68a548a4e2431c3ced21c9e19c719d2e28
Jordan Russell <jwrussell83@gmail.com>
2026-05-25 21:51:29 -0700

    Minimal next.config - remove webpack

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname),
-    };
-    return config;
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
